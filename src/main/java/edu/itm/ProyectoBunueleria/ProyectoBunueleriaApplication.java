3f0cadc906cf7afb2574502815c56bb50a2a01d5
Camilovip9610 <camilovillegas256517@correo.itm.edu.co>
2026-09-22 20:49:21 -0500

    Modificacion y cargue de archivos

---
+++ src/main/java/edu/itm/ProyectoBunueleria/ProyectoBunueleriaApplication.java
@@ -0,0 +1,13 @@
+package edu.itm.ProyectoBunueleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBunueleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBunueleriaApplication.class, args);
+	}
+
+}
